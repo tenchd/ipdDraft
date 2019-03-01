17675c3a16d4e4c37ceee57be44089c712326a47
tenchd <dmt213@gmail.com>
2019-03-01 13:30:37 -0500

    added some comments

--- play_n_plot.sh
+++ play_n_plot.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#runs an ipd tournament and plots the results in a pdf
 python ipd.py
 Rscript plot_tournament_results.R
 rm Rplots.pdf
--- plot_tournament_results.R
+++ plot_tournament_results.R
@@ -1,3 +1,4 @@
+#takes an csv file containing results of an IPD tournament and plots the scores of the submissions.  very early version, just for testing.
 library(tidyverse)
 
 ipd = read_csv("test.csv")
