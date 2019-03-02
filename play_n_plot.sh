86230305d3418e3f2e2fedc42178f78e18b48bd8
tenchd <dmt213@gmail.com>
2019-03-02 10:54:42 -0500

    incorporated new visualization with runscript

--- play_n_plot.sh
+++ play_n_plot.sh
@@ -2,6 +2,8 @@
 #runs an ipd tournament and plots the results in a pdf
 python ipd.py
 Rscript plot_tournament_results.R
+Rscript visualize_history.R
 rm Rplots.pdf
-#rm overall_results.csv
-#rm matchup_results.csv
+rm overall_results.csv
+rm matchup_results.csv
+rm game_history.csv
--- visualize_history.R
+++ visualize_history.R
@@ -4,3 +4,5 @@ hist = read_csv("game_history.csv")
 
 ggplot(data=hist) + geom_text(mapping = aes(y = round, x = player, color = move, label = move, size = 10)) + scale_y_reverse() + 
   theme(legend.position = "none", axis.text.x = element_text(size=12))
+
+ggsave(filename="sample_game.pdf")
