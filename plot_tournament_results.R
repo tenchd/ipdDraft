aa2ef8dc727601f0b4b594564d63ad8d055475c1
tenchd <dmt213@gmail.com>
2019-03-02 17:06:56 -0500

    added comments in R scripts

--- plot_tournament_results.R
+++ plot_tournament_results.R
@@ -1,13 +1,17 @@
+#author: David Tench
 #takes an csv file containing results of an IPD tournament and plots the scores of the submissions.  very early version, just for testing.
 library(tidyverse)
 
+#read in total scores for strategies, and matchup scores
 ipd = read_csv("overall_results.csv")
 match = read_csv("matchup_results.csv")
 
+#plot total scores as a bar chart
 ggplot(data=ipd) +
   geom_col(mapping = aes(x = name, y = score, fill = name)) + theme(legend.position="none")
 ggsave(filename = "overall_scores.pdf")
 
+#plot matchup scores as bar charts, organized by matchup
 ggplot(data=match) + facet_wrap(~ game_num) + geom_col(mapping = aes(x = player, fill = name, y = score)) + 
   geom_text(aes(x = player, y = 0, label = name, vjust=-0.25)) +
   theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank(), legend.position = "none")
--- visualize_history.R
+++ visualize_history.R
@@ -1,7 +1,12 @@
+#author: David Tench
+#creates a visual representation of the moves chosen by both players in an IPD game.  early version, for testing
+
 library(tidyverse)
 
+#load game history
 hist = read_csv("game_history.csv")
 
+#plot history
 ggplot(data=hist) + geom_text(mapping = aes(y = round, x = player, color = move, label = move, size = 10)) + scale_y_reverse() + 
   theme(legend.position = "none", axis.text.x = element_text(size=12))
 
